7ae2fe47a07fc3abe5087eb4d8f045cc699536f3
Victor <naragacuruga@gmail.com>
2024-11-10 16:49:57 -0600

    no c ro

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/main.c.o"
+  "CMakeFiles/Chess.dir/main.c.o.d"
+  "Chess"
+  "Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
